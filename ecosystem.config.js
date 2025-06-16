322849e4863bea50141efadf26cb1cf6ef0ea0ac
ben-natt <benny@natt.world>
2025-06-16 17:41:35 +0800

    added ecosystem.config.js to enable automatic restarts if server is down.

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    apps: [
+      {
+        name: "grafton-backend",
+        script: "server.js",
+        instances: 1,
+        autorestart: true,
+        watch: false,
+        max_memory_restart: "300M",
+        env: {
+          NODE_ENV: "production",
+          PORT: 3000
+        }
+      }
+    ]
+  };
+  
